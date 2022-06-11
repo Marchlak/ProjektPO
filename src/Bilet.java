ac72ea7d2a17f7c113a53aad71eebd48a4887cf7
DeerDancer5 <107306499+DeerDancer5@users.noreply.github.com>
2022-06-11 16:42:59 +0200

    rozwój Bilet i Flota

--- src/Bilet.java
+++ src/Bilet.java
@@ -1,2 +1,18 @@
 public class Bilet {
+    String skad, dokad;
+    //data
+
+    public Bilet(String skad, String dokad){ //i jeszcze data tu bedzie
+        this.skad = skad;
+        this.dokad = dokad;
+    }
+
+    public String getDokad() {
+        return dokad;
+    }
+
+    public String getSkad() {
+        return skad;
+    }
+
 }
