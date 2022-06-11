5aaa21f20477bdbe29bb110f2fb8d4c9bc54f26a
Marchlak <bingor1@wp.pl>
2022-06-11 15:19:12 +0200

    Zrobienie Klasy Trasa i Lotnisko

--- src/Lotnisko.java
+++ src/Lotnisko.java
@@ -1,2 +1,28 @@
 public class Lotnisko {
+    private double X;
+    private double Y;
+    private String miasto;
+
+    public Lotnisko(float x, float y, String miasto) {
+        X = x;
+        Y = y;
+        this.miasto = miasto;
+    }
+
+    public double obliczodleglosc(Lotnisko L)
+    {
+      return Math.sqrt(Math.pow(X-L.X,2)+Math.pow(Y-L.Y,2));
+      // wzór do zmiany narazie taki podstawowy
+    }
+
+    public double getY() {
+        return Y;
+    }
+    public String getMiasto() {
+        return miasto;
+    }
+    public double getX() {
+        return X;
+    }
+
 }
--- src/Trasa.java
+++ src/Trasa.java
@@ -1,2 +1,22 @@
 public class Trasa {
+private Lotnisko A,B;
+private double odleglosc;
+
+    public Trasa(Lotnisko a, Lotnisko b) {
+        A = a;
+        B = b;
+        odleglosc=A.obliczodleglosc(B);
+    }
+
+    public Lotnisko getA() {
+        return A;
+    }
+
+    public Lotnisko getB() {
+        return B;
+    }
+
+    public double getOdleglosc() {
+        return odleglosc;
+    }
 }
