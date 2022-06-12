9ba665c2675397e2648fe03be3bf883643b87d26
Marchlak <bingor1@wp.pl>
2022-06-12 18:26:22 +0200

    dobra teraz wszystko na bierzaco wysylam

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,77 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        LiniaLotnicza linia =new LiniaLotnicza();
+        int petla;
+        do {
+            System.out.println("Witaj w Systemie kontroli lotów\n");
+            System.out.println("1. Interefejs Uzytownika\n");
+            System.out.println("2. Interfejs Admina\n");
+            System.out.println("0. Wyjdz\n");
+            petla = sc.nextInt();
+            if (petla == 1)
+            {
+
+
+            }
+            else if(petla==2)
+            {
+                do
+                {
+                    case -> 1
+                            {
+
+                            }
+                    case -> 2
+                            {
+
+                            }
+                    case -> 3
+                            {
+
+                            }
+                    case -> 4
+                            {
+
+                            }
+                    case -> 5
+                            {
+
+                            }
+                    case -> 6
+                            {
+
+                            }
+                    case -> 7
+                            {
+
+                            }
+                    case -> 8
+                            {
+
+                            }
+                    case -> 9
+                            {
+
+                            }
+                    case -> 10
+                            {
+
+                            }
+
+
+
+                }while(petla!=0)
+            }
+
+
+
+        }while(petla!=0);
+
 
 
     }
