feb99b32aa9106dd86c4703b51c8367c475547c0
DeerDancer5 <107306499+DeerDancer5@users.noreply.github.com>
2022-06-13 00:03:46 +0200

    Update LiniaLotnicza.java

--- src/LiniaLotnicza.java
+++ src/LiniaLotnicza.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LiniaLotnicza {
     private ArrayList<Lot> loty;
@@ -92,5 +98,100 @@ public class LiniaLotnicza {
     public int ileTras()
     {
         return trasy.size();
+    }
+    public ArrayList <Lotnisko>  wczytajLotniska() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+
+
+        String[] linia = scanner.nextLine().split(";");
+
+
+        for(int i = 0; i< linia.length;i++) {
+            String[] parametry = linia[i].split(" ");
+            double x = Double.parseDouble(parametry[0]);
+            double y = Double.parseDouble(parametry[1]);
+            String miasto = parametry[2];
+            Lotnisko l = new Lotnisko(x, y, miasto);
+            lotniska.add(l);
+
+        }
+        return lotniska;
+    }
+    public ArrayList <Trasa> wczytajTrasy() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+
+        scanner.nextLine();
+
+        String[] linia = scanner.nextLine().split(";");
+        for(int i=0;i< linia.length;i++) {
+            String[] parametry = linia[i].split(" ");
+            int x = Integer.parseInt(parametry[0]);
+            int y = Integer.parseInt(parametry[1]);
+            Trasa trasa = new Trasa(lotniska.get(x), lotniska.get(y));
+            trasy.add(trasa);
+        }
+        return trasy;
+    }
+    public ArrayList<Lot> wczytajLoty() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+
+
+        for(int i = 0;i<2;i++){
+            scanner.nextLine();
+        }
+        String[] linia = scanner.nextLine().split(";");
+
+        for(int i=0;i< linia.length;i++) {
+            String[] parametry = linia[i].split(" ");
+            int rok = Integer.parseInt(parametry[0]);
+            int miesac = Integer.parseInt(parametry[1]);
+            int dzien = Integer.parseInt(parametry[2]);
+            int godzina = Integer.parseInt(parametry[3]);
+            int minuta = Integer.parseInt(parametry[4]);
+            int wtrasa = Integer.parseInt(parametry[5]);
+            int miejsca = Integer.parseInt(parametry[6]);
+            int nrsamolotu = Integer.parseInt(parametry[7]);
+
+            LocalDateTime czas = LocalDateTime.of(LocalDate.of(rok,miesac,dzien), LocalTime.of(godzina,minuta));
+            Lot lot = new Lot(czas,trasy.get(wtrasa),miejsca,nrsamolotu);
+            loty.add(lot);
+        }
+        return loty;
+    }
+    public ArrayList <Samolot>  wczytajSamoloty() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("input.txt"));
+
+
+        for (int i = 0; i < 3; i++) {
+            scanner.nextLine();
+        }
+        String[] linia = scanner.nextLine().split(";");
+        for (int i = 0; i < linia.length; i++) {
+            String[] parametry = linia[i].split(" ");
+            double zasieg = Double.parseDouble(parametry[0]);
+            int liczbamiejsc = Integer.parseInt(parametry[1]);
+            String model = parametry[2];
+            Samolot samolot = new Samolot(zasieg,liczbamiejsc,model);
+            samoloty.add(samolot);
+        }
+
+
+
+
+
+        return samoloty;
+    }
+    public ArrayList<Samolot> dodajLotyDoSamolotow(){
+        for(int i=0;i< loty.size();i++){
+            samoloty.get(loty.get(i).nrsamolotu).dodajLot2(loty.get(i));
+        }
+        return samoloty;
+    }
+    public void wczytaj() throws FileNotFoundException {
+        wczytajLotniska();
+        wczytajTrasy();
+        wczytajLoty();
+        wczytajSamoloty();
+        dodajLotyDoSamolotow();
     }
 }
